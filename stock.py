e9d5471a239fbbc6841cd07c9f61da6c877010c7
LDorian <lamourdorian@outlook.fr>
2021-05-01 20:26:59 +0200

    Change folder for downloaded datas

--- stock.py
+++ stock.py
@@ -5,21 +5,21 @@ import matplotlib.pyplot as plt
 import copy
 
 # Define stock & date
-stock = ['TSLA', 'MSFT']
+stock = ['PG', 'UL', 'JNJ']
 start_date = '2020-01-01'
 end_date = '2021-01-01'
 
 for v in stock:
     # Download .csv
     data = yf.download(v, start_date, end_date)
-    data.to_csv(v+'.csv')
+    data.to_csv('./Stocks/'+v+'.csv')
 
 dates = pd.date_range(start_date, end_date)
 dataFrame = pd.DataFrame(index=dates)
 
 for v in stock:
     # Put in a frame
-    data = pd.read_csv(v+'.csv', index_col="Date", parse_dates=True,
+    data = pd.read_csv('./Stocks/'+v+'.csv', index_col="Date", parse_dates=True,
                        usecols=['Date', 'Close'], na_values=['nan'])
     data = data.rename(columns={'Close': v})
 
